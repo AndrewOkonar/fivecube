2370e032758e4733f61fb79af03211431a1373b2
AndrewOkonar <andrew.okonar@gmail.com>
2021-07-25 19:11:03 +0300

    first page in progress

---
+++ dist/js/main.js
@@ -0,0 +1,24 @@
+$('#carousel1').owlCarousel({
+  autoplay:true,
+  autoplayTimeout: 10000,
+  items: 1,
+  dots: true,
+  touchDrag: false,
+  pullDrag: false,
+  loop:true
+});
+
+
+$('.help .item').on('click', function() {
+  $('.help .item').removeClass('active');
+  $(this).addClass('active');
+
+  if ($('.help .item').hasClass('web active')) {
+    $('.help').addClass('active');
+  }
+  else {
+    $('.help').removeClass('active');
+  }
+});
+
+//# sourceMappingURL=main.js.map
